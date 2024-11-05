fe620cad9676808fa023c4872b7cd5268bc7f27a
Olga Oliveira <105463799+olguinha@users.noreply.github.com>
2024-11-04 21:03:13 -0400

    novos services
    adicionando conteúdos na serviços

--- src/app/services/cursos.service.ts
+++ src/app/services/cursos.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class CursosService {
-
-  constructor() { }
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+export interface Curso {
+  _id?: string;
+  codcurso: String;
+  nomecurso: String;
+  descricao: String;
+  carga_horaria_total: Date;
+  disciplinas:String;
 }
+@Injectable({
+providedIn: 'root'
+})
+export class CursoService {
+private apiUrl = 'http://localhost:3000/alunos';
+constructor(private http: HttpClient) {}
+getCursos(): Observable<Curso[]> {
+ return this.http.get<Curso[]>(this.apiUrl);
+}
+createCurso(curso: Curso): Observable<Curso> {
+ return this.http.post<Curso>(this.apiUrl, curso);
+}
+updateCurso(curso: Curso): Observable<Curso> {
+ return this.http.put<Curso>(`${this.apiUrl}/${curso._id}`, curso);
+}
+deleteCurso(id: string): Observable<any> {
+ return this.http.delete(`${this.apiUrl}/${id}`);
+}
+}
+
