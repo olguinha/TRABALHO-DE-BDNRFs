998a4b483348a990d5b7d7e9aa90172ce87cd5f6
Olga Oliveira <105463799+olguinha@users.noreply.github.com>
2024-11-07 19:55:57 -0400

    ajuste na concatenação
    ajustes primarios

--- src/app/services/cursos.service.ts
+++ src/app/services/cursos.service.ts
@@ -2,30 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 export interface Curso {
-  _id?: string;
-  codcurso: String;
-  nomecurso: String;
-  descricao: String;
-  carga_horaria_total: Date;
-  disciplinas:  String;
+    _id?: string;
+    codcurso: String;
+    nomecurso: String;
+    descricao: String;
+    carga_horaria_total: Date;
+    disciplinas:  String;
 }
 @Injectable({
-providedIn: 'root'
+  providedIn: 'root'
 })
+
 export class CursoService {
-private apiUrl = 'http://localhost:3000/cursos';
-constructor(private http: HttpClient) {}
-getCursos(): Observable<Curso[]> {
- return this.http.get<Curso[]>(this.apiUrl);
-}
-createCurso(curso: Curso): Observable<Curso> {
- return this.http.post<Curso>(this.apiUrl, curso);
-}
-updateCurso(curso: Curso): Observable<Curso> {
- return this.http.put<Curso>(`${this.apiUrl}/${curso._id}`, curso);
-}
-deleteCurso(id: string): Observable<any> {
- return this.http.delete(`${this.apiUrl}/${id}`);
-}
+
+    private apiUrl = 'http://localhost:3000/cursos';
+    constructor(private http: HttpClient) {}
+    getCursos(): Observable<Curso[]> {
+    return this.http.get<Curso[]>(this.apiUrl);
+    }
+    createCurso(curso: Curso): Observable<Curso> {
+    return this.http.post<Curso>(this.apiUrl, curso);
+    }
+    updateCurso(curso: Curso): Observable<Curso> {
+    return this.http.put<Curso>(`${this.apiUrl}/${curso._id}`, curso);
+    }
+    deleteCurso(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+    }
 }
 
--- src/app/services/disciplinas.service.ts
+++ src/app/services/disciplinas.service.ts
@@ -1,31 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-export interface Disciplinas {
-  _id?: string;
-  coddisciplina: String,
-  nomedisciplina: String,
-  ementa: String,
-  Carga_horaria: Date,
-  professores: String
+export interface Disciplina {
+    _id?: string;
+    coddisciplina: String,
+    nomedisciplina: String,
+    ementa: String,
+    Carga_horaria: Date,
+    professores: String
 
 }
 @Injectable({
-providedIn: 'root'
+  providedIn: 'root'
 })
+
 export class DisciplinasService {
-private apiUrl = 'http://localhost:3000/displinas';
-constructor(private http: HttpClient) {}
-getDisciplinas(): Observable<Disciplinas[]> {
- return this.http.get<[]>(this.apiUrl);
-}
-createDisciplinas(disciplinas: Disciplinas): Observable<Disciplinas> {
- return this.http.post<Disciplinas>(this.apiUrl, disciplinas);
-}
-updateDisciplinas(disciplinas: Disciplinas): Observable<Disciplinas> {
- return this.http.put<Disciplinas>(`${this.apiUrl}/${disciplinas._id}`, disciplinas);
-}
-deleteDisciplinas(id: string): Observable<any> {
- return this.http.delete(`${this.apiUrl}/${id}`);
-}
+
+  private apiUrl = 'http://localhost:3000/disciplinas';
+  constructor(private http: HttpClient) {}
+  getDisciplinas(): Observable<Disciplina[]> {
+  return this.http.get<[]>(this.apiUrl);
+  }
+  createDisciplinas(disciplinas: Disciplina): Observable<Disciplina> {
+  return this.http.post<Disciplina>(this.apiUrl, disciplinas);
+  }
+  updateDisciplinas(disciplinas: Disciplina): Observable<Disciplina> {
+  return this.http.put<Disciplina>(`${this.apiUrl}/${disciplinas._id}`, disciplinas);
+  }
+  deleteDisciplinas(id: string): Observable<any> {
+  return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
 }
